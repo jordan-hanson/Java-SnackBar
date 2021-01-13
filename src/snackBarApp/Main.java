e1f3f9cfb6791ca0eb712e198eff0f2385e33555
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-01-13 09:29:48 -0700

    snack class makes a snack from main file

--- src/snackBarApp/Main.java
+++ src/snackBarApp/Main.java
@@ -2,6 +2,8 @@ package snackBarApp;
 
 public class Main {
     public static void main(String[] args) {
+        Snack snack1 = new Snack("Lays Chips", 3, 2.50);
         System.out.println("SnackBar Yum!");
+        System.out.println(snack1);
     }
 }
--- src/snackBarApp/Snack.java
+++ src/snackBarApp/Snack.java
@@ -1,13 +1,27 @@
 package snackBarApp;
 
 public class Snack {
-    private int maxId;
+    private static int maxId = 0;
     private int id;
     private String name;
     private int quantity;
-    private int cost;
-    private int vendingMachineId;
+    private double cost;
+    private static int vendingMachineId = 125;
 
+
+
+    //Constructor
+    public Snack(String name, int quantity, double cost) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+        this.vendingMachineId = vendingMachineId;
+    }
+
+
+    //Getters
     public int getId() {
         return this.id;
     }
@@ -20,10 +34,31 @@ public class Snack {
     public int getQuantity() {
         return this.quantity;
     }
-    public int getCost() {
+    public double getCost() {
         return this.cost;
     }
     public int getVendingMachineId() {
         return this.vendingMachineId;
     }
+
+    //Setters
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public void setCost(double amount){
+        this.cost = amount;
+    }
+    public void setVendingMachineId(int vendingMachineId) {
+        this.vendingMachineId = vendingMachineId;
+    }
+    public void setMaxId(int id){
+        this.maxId = id;
+    }
+
 }
